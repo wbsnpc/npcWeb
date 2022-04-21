6b4f39265d095c3ec135623e3f9b6e1b4d1740ec
npc <82518037+wbsnpc@users.noreply.github.com>
2022-04-21 16:03:45 +0800

    Delete js directory

--- js/chunk-2d0a43df.a2367bfb.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0a43df"],{"0636":function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v("404")])},c=[],l=t("2877"),a={},r=Object(l["a"])(a,u,c,!1,null,null,null);e["default"]=r.exports}}]);
-//# sourceMappingURL=chunk-2d0a43df.a2367bfb.js.map
